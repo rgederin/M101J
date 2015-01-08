46be472de12953de942dbfc4951bdbbae6d9121a
rgederin <452629996h>
2015-01-08 16:54:53 +0200

    M101J Spark hello world

---
+++ src/main/java/com/gederin/HelloWorldSpark.java
@@ -0,0 +1,24 @@
+package com.gederin;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rgederin
+ * Date: 08.01.15
+ * Time: 16:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorldSpark {
+    public static void main (String[] args){
+        Spark.get(new Route("/") {
+            @Override
+            public Object handle(Request request, Response response) {
+                return "Hello World from Spark";
+            }
+        });
+    }
+}
