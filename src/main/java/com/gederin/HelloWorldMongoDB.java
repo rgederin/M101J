c6b5a1f41a1d4d0e120b4caa862fac3dd3c75b6e
rgederin <452629996h>
2015-01-08 14:38:13 +0200

    M101J course - hello world  mongo db

---
+++ src/main/java/com/gederin/HelloWorldMongoDB.java
@@ -0,0 +1,30 @@
+package com.gederin;
+import com.mongodb.*;
+
+import java.net.UnknownHostException;
+
+
+public class HelloWorldMongoDB {
+    public static void main (String [] args) throws UnknownHostException {
+        /**
+         * Create Mongo client
+         */
+        MongoClient client = new MongoClient(new ServerAddress("localhost", 27017));
+
+        /**
+         * Get needful database
+         */
+        DB database = client.getDB("course");
+
+        /**
+         * Get needful collection
+         */
+        DBCollection collection = database.getCollection("hello");
+
+        /**
+         * Execute command
+         */
+        DBObject document = collection.findOne();
+        System.out.println(document);
+    }
+}
